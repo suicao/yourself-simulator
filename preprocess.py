81d961b7b8284543eb361e52087dea7f23d9ccc3
suicao <dandelion811@gmail.com>
2022-04-18 13:20:13 +0700

    add preprocessing script

---
+++ preprocess.py
@@ -0,0 +1,48 @@
+import json
+import re
+import os
+from tqdm.auto import tqdm
+import argparse
+
+parser = argparse.ArgumentParser(description='')
+parser.add_argument('--input_path', type=str)
+parser.add_argument('--output_path', type=str)
+parser.add_argument('--owner', type=str)
+args = parser.parse_args()
+
+
+def parse_conversation(msg_path, owner):
+    text = open(msg_path).read()
+    replaced = re.sub(r'\\u00([a-f0-9]{2})', lambda x: chr(int(x.group(1), 16)), text)
+    buffer = [ord(c) for c in replaced]
+    result = bytes(buffer).decode('utf-8')
+    m = json.loads(result)
+    participants = [x['name'] for x in m['participants']]
+    if len(participants) < 2:
+        return None
+    last_sender = ""
+    conversation = []
+    for x in m['messages'][::-1]:
+        curr_sender = x['sender_name']
+        if 'content' in x:
+            if curr_sender != last_sender:
+                msg = {"owner": curr_sender == owner, "content": x['content'].lower()}
+                conversation.append(msg)
+            else:
+                msg = conversation[-1]
+                msg["content"] = msg["content"] + ", " + x['content'].lower()
+            last_sender = curr_sender
+    return conversation
+
+
+convs = []
+for conv in tqdm(os.listdir(args.input_path)):
+    for fn in os.listdir(f'{args.input_path}/{conv}'):
+        if 'message_' in fn:
+            x = parse_conversation(msg_path=f'{args.input_path}/{conv}/{fn}',
+                                   owner=args.owner)
+            if x is not None:
+                convs.append(x)
+
+with open(args.output_path, "wb") as f:
+    json.dump(convs, f, ensure_ascii=False, indent=4)
